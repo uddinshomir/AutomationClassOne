f649c4b34a66504553ad76d99a303f81aa5ff3a5
Uddin <Uddin@DESKTOP-CBLEUAT.fios-router.home>
2020-02-02 11:29:06 -0500

    changes

---
+++ AutomationClassOne/src/main/java/com/DataTypePackage/DataType.java
@@ -0,0 +1,63 @@
+package com.DataTypePackage;
+
+public class DataType {
+
+//	This is a java line comment
+	
+	// This is another line comment. this is still a comment
+	
+	/* This
+	is 
+	a 
+	multiline
+	comment */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// integer datatype 
+		int x = 1;
+		int y = 3;
+		int j = 3;
+		x = 5; 
+	   
+		// double datatype
+		double a = 1.5; 
+		double d = 2.5; 
+		 
+		// character datatype
+		char z = 'b'; 
+		
+		//String is a non primitive data type. String is a class. 
+		
+		String name = "Shomir"; 
+		
+		
+		// Boolean Dataype
+		
+		boolean b = true; 
+		boolean c = false; 
+		
+		double h = 2; 
+		
+		System.out.println(h); 
+		System.out.println("My name is" + " " +  name); 
+		System.out.println("My name is" + " " +  name); 
+		System.out.println("My name is" + " " +  name); 
+		
+	}
+	
+	
+	public static void Addition() {
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
+
+
